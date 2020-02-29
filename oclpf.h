295e1e713c1e4a5ddcd1548c7f5d05bf1e142118
cojeap <cojeap@yahoo.com>
2020-02-29 21:22:54 +0200

    refresh 2

--- oclpf.h
+++ oclpf.h
@@ -9,20 +9,6 @@
 #include "Data_Bank/Data_Bank.h"
 #include "Power_System/Power_System.h"
 
- DEFUN_DLD (helloworld, args, nargout,
-                "Hello World Help String")
-     {
-       octave_stdout << "Hello World has "
-                     << args.length () << " input arguments and "
-                     << nargout << " output arguments.\n";
-     
-       // Return empty matrices for any outputs
-       octave_value_list retval (nargout);
-       for (int i = 0; i < nargout; i++)
-         retval(i) = octave_value (Matrix ());
-     
-       return retval;
-     }
 
 
 #endif //OPENCL_POWERFLOW_MATLAB_INTERCONNECT_H
